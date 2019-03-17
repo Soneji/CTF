b46800222f218a021ab0967c88f7f225d7f3ba65
overclockedllama <dhaval@debian-server.home>
2019-03-17 00:52:14 +0000

    fix pull ting

--- pull.php
+++ pull.php
@@ -2,7 +2,7 @@
 
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
-if ( $_POST['payload'] ) {
+if ( $_POST["payload"] ) {
 	shell_exec( "cd /var/www/ctf && git reset --hard HEAD && git pull" );
 }
 
