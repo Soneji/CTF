72c4ce70b4e1ebe6ee8e6c180b16998d435a7639
overclockedllama <dhaval@debian-server.home>
2019-03-17 00:50:54 +0000

    fix pull.php

--- pull.php
+++ pull.php
@@ -3,7 +3,7 @@
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-	shell_exec( ‘cd /var/www/ctf && git reset –hard HEAD && git pull’ );
+	shell_exec( "cd /var/www/ctf && git reset --hard HEAD && git pull" );
 }
 
 ?>
